4178270cf1bd7a9145cad34440aaac14d245990b
fasfafal <nogulin0530@gmail.com>
2024-05-12 23:05:52 +0900

    yarnの設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,8 @@
 set -o errexit
 
 bundle install
+yarn install
+yarn build #jsファイルをesbuildでバンドルしているため
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
