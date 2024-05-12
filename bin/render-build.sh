af97fca76c9514788910fe3c39c5e0081ab47ae3
fasfafal <nogulin0530@gmail.com>
2024-05-13 00:23:36 +0900

    dartsassを入れる

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@ set -o errexit
 
 bundle install
 yarn install
-#yarn build jsファイルをesbuildでバンドルしているため
+yarn build jsファイルをesbuildでバンドルしているため
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
